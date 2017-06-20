d1546f215a2cbd8f80ca7e5d3cfe28722e668a1b
deb <debkbanerji@gmail.com>
2017-06-19 22:51:05 -0400

    Added Logging, Changed Angular CLI Dependency Location

--- server.js
+++ server.js
@@ -8,39 +8,49 @@ const bodyParser = require('body-parser');
 const api = require('./server/routes/api');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use(function(error, req, res, next) {
-	console.log(req.originalUrl, ':', error.stack);
-	res.render('500', { status: 500, url: req.url, title: 'Something broke :(', user: req.user, stateMessage: '', pageState: '' });
-});
-
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
-
-// Set our api routes
-app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
 
 /**
  * Get port from environment and store in Express.
  */
 const port = process.env.PORT || '3000';
 app.set('port', port);
+console.log("Node server running on port " + port)
 
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+console.log("Serving static from dist");
+
+app.use(function(error, req, res, next) {
+	console.log(req.originalUrl, ':', error.stack);
+	res.render('500', { status: 500, url: req.url, title: 'Something broke :(', user: req.user, stateMessage: '', pageState: '' });
+});
+
+console.log("Using error logging");
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+console.log("Using body parser");
+
+// Set our api routes
+app.use('/api', api);
+
+console.log("Setting api routes");
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+console.log("Catching all other routes and return the index file");
 
 /**
  * Create HTTP server.
  */
 const server = http.createServer(app);
 
+console.log("Created Server");
 /**
  * Listen on provided port, on all network interfaces.
  */
--- server/routes/api.js
+++ server/routes/api.js
@@ -6,15 +6,22 @@ const mustache = require('mustache');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+console.log('Running api.js');
+
 const router = express.Router();
 
+console.log('Set express router');
+
 let logProjectsMade = false;
+
+console.log('Using body parser');
 
 router.use( bodyParser.json() );       // to support JSON-encoded bodies
 router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+console.log('Defining Functions');
 
 blob_extensions = ['ico', 'jpg', 'png', 'svg'];
 const templateFolder = './templates';
@@ -68,6 +75,8 @@ function readFile(baseDir, dir, fileName) {
         });
     }
 }
+
+console.log('Reading Directory');
 
 readDir(templateFolder, '/');
 
@@ -136,4 +145,6 @@ function toTitleCase(str)
 function toKebabCase(str) {
     return str.replace(/\s+/g, '-').toLowerCase();
 }
+
+console.log('Exporting router');
 module.exports = router;
